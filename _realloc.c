75dd024910998bb40fe684af5689f28c067a9623
rozyrocks <105121305+rozyrocks@users.noreply.github.com>
2022-08-10 20:38:08 +0100

    Update _realloc.c

--- _realloc.c
+++ _realloc.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: void pointer
@@ -6,6 +7,7 @@
  * @new_size: unsigned int
  * Return: pointer
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *ret = 0;
